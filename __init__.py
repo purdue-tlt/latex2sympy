88eb1e0a42beb62c41601c9d5154253c3e712a8d
NeErAj KuMaR <sainineeraj1234@gmail.com>
2020-09-09 15:07:10 +0530

    Fixed

--- __init__.py
+++ __init__.py
@@ -1,8 +1,16 @@
 import latex2sympy
 
-a = "1+2-3+\\frac{4}{5}+6^7+\sqrt{5}+6+\\variable{DEMO_{a,b,c}}"
+a = r"1+2-3\cdot\frac{4}{5}+6^7+\sqrt{5}+6+\variable{DEMO_{abc,bca,cde}}"
+a = r"e^5"
+a = r"3\in\left\{1,2,3,4,5\right\}"
 
 b = latex2sympy.process_sympy(a)
 print(b)
 for i in b.free_symbols:
-	print(i)
+	if "_" in i.name:
+		variable, dimension = i.name.split('_')
+		dimension = dimension[1: len(dimension) - 1]
+		dimension = dimension.split(",")
+		print("Variable", variable, ", Dimension", dimension)
+
+print(b.evalf(4))
