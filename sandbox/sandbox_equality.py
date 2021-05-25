caa37a1056bbeb7c50a5e4e975f946f3f7288896
Will Grauvogel <builtbywill@gmail.com>
2021-05-25 17:30:14 -0400

    sandbox test updates

--- sandbox/sandbox_equality.py
+++ sandbox/sandbox_equality.py
@@ -73,11 +73,17 @@ def get_sample_diff(expr1, expr2):
         return 'ERROR'
 
     sample_values = [
+        # -1000,
         # -100,
+        # -10,
         # -1,
+        # -0.5,
         0,
+        0.5,
         1,
-        100
+        10,
+        100,
+        1000
     ]
     values_per_symbol = []
     symbols_list = []
@@ -101,7 +107,15 @@ def get_sample_diff(expr1, expr2):
         results_1.append(result_1)
         results_2.append(result_2)
 
-    error_percentage = 100 * abs(sum(results_1) - sum(results_2)) / sum(results_1)
+    sum_1 = sum(results_1)
+    sum_2 = sum(results_2)
+    abs_diff = abs(sum_1 - sum_2)
+
+    if abs_diff == 0:
+        return 0
+
+    error_ratio = abs_diff / sum_1
+    error_percentage = 100 * error_ratio
 
     return error_percentage
 
@@ -123,10 +137,10 @@ def compare(correct_answer, student_answer):
     print('\tdiff =>', equals_diff)
     print('')
 
-    simplify_result = simplify(correct_answer_parsed - student_answer_parsed)
-    print('Symbolic using simplify(), (simplify(c - a) == 0) =>', simplify_result == 0)
-    print('\tsimplified =>', simplify_result)
-    print('')
+    # simplify_result = simplify(correct_answer_parsed - student_answer_parsed)
+    # print('Symbolic using simplify(), (simplify(c - a) == 0) =>', simplify_result == 0)
+    # print('\tsimplified =>', simplify_result)
+    # print('')
 
     sample_result = get_sample_diff(correct_answer_parsed, student_answer_parsed)
     print('Sampled =>', sample_result)
@@ -143,15 +157,15 @@ def compare(correct_answer, student_answer):
     correct_answer_replaced = replace_rationals(correct_answer_parsed, r)
     student_answer_replaced = replace_rationals(student_answer_parsed, r)
 
-    print('Double Equals w/o Rationals (replace_rationals(c) == replace_rationals(a)) =>', correct_answer_replaced == student_answer_replaced)
+    # print('Double Equals w/o Rationals (replace_rationals(c) == replace_rationals(a)) =>', correct_answer_replaced == student_answer_replaced)
+    # print('')
+
+    # print('.equals() w/o Rationals (replace_rationals(c).equals(replace_rationals(a))) =>', correct_answer_replaced.equals(student_answer_replaced))
+    # print('')
+
+    print('Form w/o Numbers using srepr(), (srepr(replace_rationals(c)) == srepr(replace_rationals(a))) =>', srepr(correct_answer_replaced) == srepr(student_answer_replaced))
     print('\treplace_rationals(c):', correct_answer_replaced)
     print('\treplace_rationals(a):', student_answer_replaced)
-    print('')
-
-    print('.equals() w/o Rationals (replace_rationals(c).equals(replace_rationals(a))) =>', correct_answer_replaced.equals(student_answer_replaced))
-    print('')
-
-    print('String Rep w/o Rationals - String Rep (srepr(replace_rationals(c)) == srepr(replace_rationals(a))) =>', srepr(correct_answer_replaced) == srepr(student_answer_replaced))
     print('\tsrepr(replace_rationals(c)):', srepr(correct_answer_replaced))
     print('\tsrepr(replace_rationals(a)):', srepr(student_answer_replaced))
 
@@ -163,78 +177,78 @@ def compare(correct_answer, student_answer):
 #
 
 answer_sets = [
-    # {
-    #     'correct_answer': '(x-y)(x+2y)',
-    #     'student_answers': [
-    #         'x^2+xy-2y^2',
-    #         '(x-y)(x+2y)',
-    #         '(x+2y)(x-y)',
-    #         '(2\\times y+x)(-y+x)',
-    #         '(y\\cdot 2+x)(-y+x)'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '2\\pi \\variable{r}^2',
-    #     'student_answers': [
-    #         '2\\pi \\variable{r}^2',
-    #         '\\pi 2\\variable{r}^2',
-    #         '2\\times \\pi \\times \\variable{r}^2',
-    #         '2\\pi \\variable{r} \\times \\variable{r}'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '2x - 3y',
-    #     'student_answers': [
-    #         '-3y + 2x'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': 'x\\times x',
-    #     'student_answers': [
-    #         'x\\times x',
-    #         'x\\cdot x',
-    #         'x^2',
-    #         '(\\sqrt{x})^{4}'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '23e^{-1\\times \\sqrt{t^2}}',
-    #     'student_answers': [
-    #         '23e^{-t}'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': 'a=x^2+1',
-    #     'student_answers': [
-    #         'x^2+1=a'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '99.9x',
-    #     'student_answers': [
-    #         '99.86x'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '\\frac{(1+1)\\cdot (100-20-5\\cdot w_t)+0.8\\cdot (340-5-1\\cdot w_c)}{(1+1)\\cdot (1+3)-0.8\\cdot 0.8}',
-    #     'student_answers': [
-    #         '\\frac{428 - 0.8w_c - 10w_t}{7.36}',
-    #         '\\frac{2675}{46} - \\frac{5}{46}w_c - \\frac{125}{92}w_t',
-    #         '58.15220 - 0.1087w_c - 1.3587w_t'
-    #     ]
-    # },
-    # {
-    #     'correct_answer': '\\frac{2675}{46} - \\frac{5}{46}w_c - \\frac{125}{92}w_t',
-    #     'student_answers': [
-    #         # '\\frac{(1+1)\\cdot (100-20-5\\cdot w_t)+0.8\\cdot (340-5-1\\cdot w_c)}{(1+1)\\cdot (1+3)-0.8\\cdot 0.8}',
-    #         # '\\frac{428 - 0.8w_c - 10w_t}{7.36}',
-    #         '58.15220 - 0.1087w_c - 1.3587w_t',
-    #         '58.15220 - 1.3587w_t - 0.1087w_c',
-    #         # '58.15220 - 0.1087w_c - 1.3587w_t + x',
-    #         # '58.15220 - 0.1087w_c + x',
-    #         # '58.15220 - 0.1087w_c'
-    #     ]
-    # }
+    {
+        'correct_answer': '(x-y)(x+2y)',
+        'student_answers': [
+            'x^2+xy-2y^2',
+            '(x-y)(x+2y)',
+            '(x+2y)(x-y)',
+            '(2\\times y+x)(-y+x)',
+            '(y\\cdot 2+x)(-y+x)'
+        ]
+    },
+    {
+        'correct_answer': '2\\pi \\variable{r}^2',
+        'student_answers': [
+            '2\\pi \\variable{r}^2',
+            '\\pi 2\\variable{r}^2',
+            '2\\times \\pi \\times \\variable{r}^2',
+            '2\\pi \\variable{r} \\times \\variable{r}'
+        ]
+    },
+    {
+        'correct_answer': '2x - 3y',
+        'student_answers': [
+            '-3y + 2x'
+        ]
+    },
+    {
+        'correct_answer': 'x\\times x',
+        'student_answers': [
+            'x\\times x',
+            'x\\cdot x',
+            'x^2',
+            '(\\sqrt{x})^{4}'
+        ]
+    },
+    {
+        'correct_answer': '23e^{-1\\times \\sqrt{t^2}}',
+        'student_answers': [
+            '23e^{-t}'
+        ]
+    },
+    {
+        'correct_answer': 'a=x^2+1',
+        'student_answers': [
+            'x^2+1=a'
+        ]
+    },
+    {
+        'correct_answer': '99.9x',
+        'student_answers': [
+            '99.86x'
+        ]
+    },
+    {
+        'correct_answer': '\\frac{(1+1)\\cdot (100-20-5\\cdot w_t)+0.8\\cdot (340-5-1\\cdot w_c)}{(1+1)\\cdot (1+3)-0.8\\cdot 0.8}',
+        'student_answers': [
+            '\\frac{428 - 0.8w_c - 10w_t}{7.36}',
+            '\\frac{2675}{46} - \\frac{5}{46}w_c - \\frac{125}{92}w_t',
+            '58.15220 - 0.1087w_c - 1.3587w_t'
+        ]
+    },
+    {
+        'correct_answer': '\\frac{2675}{46} - \\frac{5}{46}w_c - \\frac{125}{92}w_t',
+        'student_answers': [
+            # '\\frac{(1+1)\\cdot (100-20-5\\cdot w_t)+0.8\\cdot (340-5-1\\cdot w_c)}{(1+1)\\cdot (1+3)-0.8\\cdot 0.8}',
+            # '\\frac{428 - 0.8w_c - 10w_t}{7.36}',
+            '58.15220 - 0.1087w_c - 1.3587w_t',
+            '58.15220 - 1.3587w_t - 0.1087w_c',
+            # '58.15220 - 0.1087w_c - 1.3587w_t + x',
+            # '58.15220 - 0.1087w_c + x',
+            # '58.15220 - 0.1087w_c'
+        ]
+    },
     {
         'correct_answer': '\\frac{2675}{46}x',
         'student_answers': [
