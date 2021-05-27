03ce681eb85787ecdf6894f32cb7a526906a5a8a
Will Grauvogel <builtbywill@gmail.com>
2021-05-27 15:51:20 -0400

    add FRACTION_NUMBER and SCI_NOTATION_NUMBER parsing grammar

--- latex2sympy.py
+++ latex2sympy.py
@@ -477,6 +477,34 @@ def convert_atom(atom):
             return sr
         except (TypeError, ValueError):
             return sympy.Number(s)
+    elif atom.SCI_NOTATION_NUMBER():
+        s = atom.SCI_NOTATION_NUMBER().getText()
+        s_parts = s.split('\\times 10^')
+        s1 = s_parts[0].replace(',', '')
+        try:
+            n1 = sympy.Rational(s1)
+        except (TypeError, ValueError):
+            n1 = sympy.Number(s1)
+        s2 = s_parts[1].replace('{', '').replace(',', '').replace('}', '')
+        try:
+            n2 = sympy.Rational(s2)
+        except (TypeError, ValueError):
+            n2 = sympy.Number(s2)
+        n_exp = sympy.Mul(n1, sympy.Pow(10, n2))
+        try:
+            n = sympy.Rational(n_exp)
+        except (TypeError, ValueError):
+            n = sympy.Number(n_exp)
+        return n
+    elif atom.FRACTION_NUMBER():
+        s = atom.FRACTION_NUMBER().getText().replace("\\frac{", "").replace("}{", "/").replace("}", "").replace(",", "")
+        try:
+            sr = sympy.Rational(s)
+            return sr
+        except ZeroDivisionError:
+            return sympy.S.EmptySet
+        except (TypeError, ValueError):
+            return sympy.Number(s)
     elif atom.E_NOTATION():
         s = atom.E_NOTATION().getText().replace(",", "")
         try:
