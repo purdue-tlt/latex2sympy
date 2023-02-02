9b4d2676509e2f725ef35f4a554b4a84cffc2e85
Will Grauvogel <builtbywill@gmail.com>
2023-02-02 15:09:08 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import find_packages
 from skbuild import setup
 
 setup(
     version="1.6.1",
     name="latex2sympy",
-    packages=find_packages(include=['latex2sympy', 'latex2sympy.*']),
+    packages=['latex2sympy', 'latex2sympy.parser', 'latex2sympy.parser.python'],
     package_dir={'': 'src'},
     cmake_install_dir='src/latex2sympy'
 )
