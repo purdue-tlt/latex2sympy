ba54762cde44e2682890333e927d49259cb08283
Will Grauvogel <builtbywill@gmail.com>
2023-02-06 13:56:10 -0500

    remove old packages from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from skbuild import setup
 setup(
     version="1.6.1",
     name="latex2sympy",
-    packages=['latex2sympy', 'latex2sympy.parser', 'latex2sympy.parser.python'],
+    packages=['latex2sympy'],
     package_dir={'': 'src'},
     cmake_install_dir='src/latex2sympy'
 )
