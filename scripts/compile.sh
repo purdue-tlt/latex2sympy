a5fafff62c79e3355266e27193c2be9b579152e8
Will Grauvogel <builtbywill@gmail.com>
2023-02-07 10:39:26 -0500

    compile script improvements

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,11 +6,21 @@ rel_path="$(dirname "$rdir")"
 # Change to that path and run the file
 cd $rel_path
 
+echo ''
+# Activate virtual environment
+echo "activating venv..."
+if test -f .env/bin/activate
+then source .env/bin/activate && echo "venv activate (bin)"
+elif test -f .env/Scripts/activate
+then source .env/Scripts/activate && echo "venv activated (Scripts)"
+else exit 1
+fi
+
 # get the current os and arch
 os="$(uname -s)"
 arch="$(uname -m)"
 out_dir="$(realpath ..)/src/latex2sympy"
-conan_setting_arch=$os
+conan_setting_arch=$arch
 generator="Unix Makefiles"
 
 # update the output dir based on os and arch
@@ -29,12 +39,15 @@ else
 fi
 
 echo ''
-echo "generate and compile cpp parser..."
+echo "generate and compile cpp parser (os: $os, arch: $arch, generator: $generator, conan_setting_arch: $conan_setting_arch, out_dir: $out_dir)..."
 rm -rf build
 mkdir build
 cd build
 cmake .. -G "$generator" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$out_dir" -DCONAN_SETTING_ARCH="$conan_setting_arch"
 make install
 echo "cpp parser generated and compiled"
+
+rm -rf build
+echo "build files cleaned"
 
 exit 0
