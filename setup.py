1e183cd1e9702c4f2b3bd036f08b5b44e57390b9
Will Grauvogel <builtbywill@gmail.com>
2023-02-07 10:39:14 -0500

    remove scikit-build from setup.py, use pre-compiled packages

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
-from skbuild import setup
+from setuptools import setup
 
 setup(
     version="1.6.1",
     name="latex2sympy",
-    packages=['latex2sympy'],
-    package_dir={'': 'src'},
-    cmake_install_dir='src/latex2sympy'
+    packages=[
+        'latex2sympy',
+        'latex2sympy.parser',
+        'latex2sympy.parser.linux',
+        'latex2sympy.parser.macOS',
+        'latex2sympy.parser.macOS.arm64',
+        'latex2sympy.parser.macOS.x86_64',
+        'latex2sympy.parser.windows'],
+    package_dir={
+        'latex2sympy': 'src/latex2sympy',
+        'latex2sympy.parser.linux': 'src/latex2sympy/parser/linux',
+        'latex2sympy.parser.macOS.arm64': 'src/latex2sympy/parser/macOS/arm64',
+        'latex2sympy.parser.macOS.x86_64': 'src/latex2sympy/parser/macOS/x86_64',
+        'latex2sympy.parser.macOS.windows': 'src/latex2sympy/parser/windows'
+    },
+    package_data={
+        'latex2sympy': [],
+        'latex2sympy.parser.linux': ['latex2antlrJson.so'],
+        'latex2sympy.parser.macOS.arm64': ['latex2antlrJson.so'],
+        'latex2sympy.parser.macOS.x86_64': ['latex2antlrJson.so'],
+        'latex2sympy.parser.windows': ['latex2antlrJson.so']
+    },
+    install_requires=[
+        'sympy==1.10.1'
+    ]
 )
