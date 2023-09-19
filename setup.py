4ee86fb6683c100f9806bf2f7fe353b342abe53b
Will Grauvogel <builtbywill@gmail.com>
2023-09-19 09:39:41 -0400

    update setup packages

--- setup.py
+++ setup.py
@@ -9,18 +9,24 @@ setup(
         'latex2sympy.lib.linux',
         'latex2sympy.lib.macOS',
         'latex2sympy.lib.macOS.arm64',
-        'latex2sympy.lib.macOS.x86_64'],
+        'latex2sympy.lib.macOS.x86_64',
+        'latex2sympy.units',
+        'latex2sympy.utils'],
     package_dir={
         'latex2sympy': 'src/latex2sympy',
         'latex2sympy.lib.linux': 'src/latex2sympy/lib/linux',
         'latex2sympy.lib.macOS.arm64': 'src/latex2sympy/lib/macOS/arm64',
-        'latex2sympy.lib.macOS.x86_64': 'src/latex2sympy/lib/macOS/x86_64'
+        'latex2sympy.lib.macOS.x86_64': 'src/latex2sympy/lib/macOS/x86_64',
+        'latex2sympy.units': 'src/latex2sympy/units',
+        'latex2sympy.utils': 'src/latex2sympy/utils'
     },
     package_data={
         'latex2sympy': [],
         'latex2sympy.lib.linux': ['latex2antlrJson.so'],
         'latex2sympy.lib.macOS.arm64': ['latex2antlrJson.so'],
-        'latex2sympy.lib.macOS.x86_64': ['latex2antlrJson.so']
+        'latex2sympy.lib.macOS.x86_64': ['latex2antlrJson.so'],
+        'latex2sympy.units': [],
+        'latex2sympy.utils': []
     },
     install_requires=[
         'sympy==1.12'
