a7ba62e903dcb7d91f1b5f428f1c1ab9f6b67cbf
Jason Dufair <jase@purdue.edu>
2023-10-06 13:52:42 -0500

    language update

--- tests/grammar_build_test.py
+++ tests/grammar_build_test.py
@@ -10,7 +10,7 @@ def test_ensure_grammar_file_is_built():
     grammar_file.close()
     file_hash = h.hexdigest()
     assert file_hash == '8532f17fe8000457059e7d1f8f7da18b73fffecf8a5b7830dab0bbb7d9f199b8', \
-        'LATEX.g4 has changed. Please run the build.sh script then update the hash' + \
+        'LATEX.g4 has changed. Please run the compile.sh script for all architectures then update the hash' + \
         f'in this test to {file_hash}'
 
 
@@ -22,5 +22,5 @@ def test_ensure_cpp_file_is_built():
     grammar_file.close()
     file_hash = h.hexdigest()
     assert file_hash == '0d3b8aa0570d245303392be38b73bcff8b019272e43041a170e46ee3efac9cc0', \
-        'latex2antlrJson.cpp has changed. Please run the build.sh script then update the hash' + \
+        'latex2antlrJson.cpp has changed. Please run the compile.sh script for all architectures then update the hash' + \
         f'in this test to {file_hash}'
